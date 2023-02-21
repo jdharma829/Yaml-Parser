da657cb997ec7e06722aa169ae6b093c282ab136
jdharma829 <janjanam.dharma@gmail.com>
2023-02-21 14:57:51 -0700

    Add project files.

---
+++ YamlParser/DBQuery.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace YamlParser
+{
+    public class DBQuery
+    {
+        protected void CheckJSON(ref dynamic prop, dynamic value)
+        {
+            if (value is String)
+            {
+                try
+                {
+                    prop = JObject.Parse(value);
+                }
+                catch (Exception e)
+                {
+                    prop = value;
+
+                }
+            }
+            else
+                prop = value;
+        }
+
+        public struct ListQueryModeCode
+        {
+            public const string ListWithCount = "LC";
+            public const string ListNoCount = "LNC";
+            public const string Scalar = "S";
+        }
+
+        public string QueryName { get; set; }
+
+        // this is a multi line SQL query with line feeds .
+        public string QuerySql { get; set; }
+        public string? Permissions { get; set; }
+
+        // multiline value each line has one permission id
+        public List<string> PermissionList { get; set; }
+
+        //comma separated list of fields
+        public string? DefaultSortOrder { get; set; }
+
+        //multiline text
+        public string? Description { get; set; }
+
+        public string? ModeCode { get; set; }
+
+
+        private dynamic? _parameterConfig, _columnConfig;
+
+        //json
+        public dynamic? ParameterConfig
+        {
+            get { return _parameterConfig; }
+            set { CheckJSON(ref _parameterConfig, value); }
+        }
+
+        //json
+        public dynamic? ColumnConfig
+        {
+            get { return _columnConfig; }
+            set { CheckJSON(ref _columnConfig, value); }
+        }
+    }
+}
---
+++ YamlParser/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using YamlParser;
+
+//DBQuery query = new DBQuery()
+//{
+//    QueryName = "Query Name",
+//    QuerySql = "Query Sql",
+//    Permissions = null,
+//    PermissionList = new List<string>() { "first level permission", "second level permission" },
+//    DefaultSortOrder = "name, price",
+//    Description = "Description Value Description Value Description Value Description \n Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value Description Value",
+//    ModeCode = null,
+//    ParameterConfig = @"{ ""name"": ""Dharma"", ""place"": ""Dharma"" }",
+//    ColumnConfig = @"{ ""place"": ""Albuquerque"", ""address"": ""Albuquerque"" }"
+//};
+
+//var updatedyaml = YamlConvert.SerializeObject(query);
+
+string yamlContent =  File.ReadAllText("yaml-sample.yaml");
+
+Console.WriteLine(yamlContent);
+
+DBQuery result = YamlConvert.DeserializeObject<DBQuery>(yamlContent);
+
+Console.Read();
